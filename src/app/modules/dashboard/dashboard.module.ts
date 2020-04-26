8ba7a2a4b33236844c5450c258d1794d5ff86973
kwamkert <tomriddle477@gmail.com>
2020-04-26 20:51:57 +0000

    dashboad module updated

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AdminComponent } from './components/admin/admin.component';
 import { CollectorComponent } from './components/collector/collector.component';
 import { OwnerComponent } from './components/owner/owner.component';
-
+import { DashboardRoutingModule } from './dashboard-routing';
 
 
 @NgModule({
   declarations: [AdminComponent, CollectorComponent, OwnerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    DashboardRoutingModule  
   ]
 })
 export class DashboardModule { }
