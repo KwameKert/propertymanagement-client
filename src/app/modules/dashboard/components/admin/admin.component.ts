2aff56980a40eb89cb07309fd1b5873058d95864
kwamkert <tomriddle477@gmail.com>
2020-05-02 19:58:12 +0000

    google maps added

--- src/app/modules/dashboard/components/admin/admin.component.ts
+++ src/app/modules/dashboard/components/admin/admin.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MapInfoWindow, MapMarker} from '@angular/google-maps';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
+  @ViewChild(MapInfoWindow, {static: false}) infoWindow: MapInfoWindow;
+
+  center = {lat: 5.5557, lng: -0.1963};
+  markerOptions = {draggable: false};
+  markerPositions: google.maps.LatLngLiteral[] = [];
+  zoom = 4;
+  display?: google.maps.LatLngLiteral;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  addMarker(event: google.maps.MouseEvent) {
+    this.markerPositions.push(event.latLng.toJSON());
+  }
+
+  move(event: google.maps.MouseEvent) {
+    this.display = event.latLng.toJSON();
+  }
+
+  openInfoWindow(marker: MapMarker) {
+    this.infoWindow.open(marker);
+  }
+
+  removeLastMarker() {
+    this.markerPositions.pop();
+  }
+
 
 }
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -4,13 +4,17 @@ import { AdminComponent } from './components/admin/admin.component';
 import { CollectorComponent } from './components/collector/collector.component';
 import { OwnerComponent } from './components/owner/owner.component';
 import { DashboardRoutingModule } from './dashboard-routing';
+import {GoogleMapsModule} from '@angular/google-maps';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
   declarations: [AdminComponent, CollectorComponent, OwnerComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule  
+    DashboardRoutingModule,
+    GoogleMapsModule,
+    MatCardModule
   ]
 })
 export class DashboardModule { }
