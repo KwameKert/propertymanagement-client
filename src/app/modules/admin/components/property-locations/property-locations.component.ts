39479afb3b9405862afda70f6971c479307f377f
kwamkert <tomriddle477@gmail.com>
2020-05-08 01:01:51 +0000

    locations plotted

--- src/app/modules/admin/components/property-locations/property-locations.component.ts
+++ src/app/modules/admin/components/property-locations/property-locations.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { CrudService } from 'src/app/modules/shared/service';
+import { MAR } from '@angular/material/core';
 
 @Component({
   selector: 'app-property-locations',
@@ -14,35 +16,61 @@ export class PropertyLocationsComponent implements OnInit {
   center = {lat: 5.559459, lng: -0.215029};
   markerOptions = {draggable: false};
   markers: Array<any> = [];
-  markerPositions: google.maps.LatLngLiteral[] = [
-    {lat: 5.597509500000001, lng:-0.2355881000000001}
-  ];
+  markerPositions: google.maps.LatLngLiteral[] ;
   zoom = 10;
   display?: google.maps.LatLngLiteral;
   infoContent = 'Hello'
 
-  constructor() { }
+  constructor(private _crudService: CrudService) { }
 
   ngOnInit(): void {
+    this.loadProperties();
+    
+  }
+
+
+  loadProperties(){
+    this._crudService.fetchAll("property").subscribe(data=>{
+      
+      let result = data.data
+    
+      let arr = [];
+      for(let item of result){
+          this.addMarker(item);
+          //arr.push({lat: item.latitude, lng:item.longitude})
+      }
+
+      
+    }, error=>{
+
+    })
+
+    console.log(this.markers)
+  }
+
+
+  addMarker(data: any){
     this.markers.push({
       position: {
-        lat:  5.597509500000001,
-        lng: -0.2355881000000001,
+        lat:  data.latitude,
+        lng: data.longitude,
       },
       label: {
         color: 'red',
         text: 'Marker label ' + (this.markers.length + 1),
       },
       title: 'Marker title ' + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
+      info: {
+        propNo: data.propNo,
+        owner: data.owner,
+        value: data.value,
+        rate: data.rate
+      },
       options: {
-      //  animation: google.maps.Animation.BOUNCE,
       },
     })
+    console.log(this.markers)
   }
-
-
-
  
 
   openInfoWindow(marker: MapMarker, content) {
