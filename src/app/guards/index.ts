3ef15244b051d5eff9801f86c741dbf51a8c1ad4
kwamkert <tomriddle477@gmail.com>
2020-05-20 22:37:44 +0000

    guards added

--- src/app/guards/index.ts
+++ src/app/guards/index.ts
@@ -1,1 +1,4 @@
 export {AuthGuard} from "./components/auth.guard";
+export {AdminGuard} from "./components/admin.guard";
+export {OwnerGuard} from "./components/owner.guard";
+export {CollectorGuard} from "./components/collector.guard";
